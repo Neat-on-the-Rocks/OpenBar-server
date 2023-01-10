d9b003c5a4379f59e2aa71a4cb9b0c96d8d3a9ae
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-10 17:41:30 -0600

    Configure file storage

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
 app.use(morgan("common"))
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))) //Set directory of assets
+
+/*Configure File Storage*/
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets")
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage})
+
