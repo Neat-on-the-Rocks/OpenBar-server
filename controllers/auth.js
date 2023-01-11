ceb3ed706851bea2025ff5141454f123b31a04a5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-11 17:10:57 -0600

    Create register user auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+import User from '../models/User';
+
+/*Register User*/
+export const register = async (req, res) => {
+    try{
+        const { firstName, lastName, email, password, pictruePath, friends, location, occupation } = req.body
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            firstName, 
+            lastName, 
+            email, 
+            password: passwordHash, 
+            pictruePath, 
+            friends, 
+            location, 
+            occupation,
+            viewedProfile: 0, //Work on this later
+            impressions: 0, //Work on this later
+        })
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser) //201 create
+    } catch (err){
+        res.status(500).json({error: err.message})
+    }
+}
