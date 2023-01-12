ed8b4727b5436311c8ea1551979cce4b427c2f4c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-11 19:35:52 -0600

    Create login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import User from "../models/User.js";
+import jwt from "jsonwebtoken"
 
 /*Register User*/
 export const register = async (req, res) => {
@@ -25,6 +26,29 @@ export const register = async (req, res) => {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser) //201 create
     } catch (err){
+        res.status(500).json({error: err.message})
+    }
+}
+
+/*Login User*/ 
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email: email})
+        if(!user){
+            res.status(400).json({msg: "User does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) {
+            return res.status(400).json({msg: "Invalid email or password"})
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        delete user.password
+        res.status(200).json({token, user})
+    } catch(err) {
         res.status(500).json({error: err.message})
     }
 }
