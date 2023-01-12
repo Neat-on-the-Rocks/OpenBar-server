9d11f73fc82e2454c05f857a7518ba60dd6afe2f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-11 21:24:07 -0600

    Create controller functions

---
+++ controllers/posts.js
@@ -0,0 +1,26 @@
+
+export const createPost = async (req, res) => {
+
+}
+
+export const getFeedPosts = async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+}
+export const getUserPosts = async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+}
+export const likePost = async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+}
