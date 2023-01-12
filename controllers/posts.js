6db792eeaeba89818ba5da63ffd957f246c13eef
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-11 22:52:24 -0600

    Write posts controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,18 +1,45 @@
+import Post from "../models/Post.js"
+import User from "../models/User.js"
 
 export const createPost = async (req, res) => {
+    try {
+        const {userId, description, picturePath} = req.body
+        const user = await User.findById(userId)
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            loaction: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
+        })
+        await newPost.save();
 
+        const post = await Post.find(); //Updates feed
+        res.status(201).json(post)
+    } catch (err) {
+        res.status(409).json({message: err.message})
+    }
 }
+
+/*READ POSTS*/
 
 export const getFeedPosts = async (req, res) => {
     try {
-
+        const post = await Post.find();
+        res.status(200).json(post)
     } catch (err) {
         res.status(404).json({message: err.message})
     }
 }
 export const getUserPosts = async (req, res) => {
     try {
-
+        const {userId} = req.params;
+        const post = await Post.find({userId})
+        res.status(200).json(post)
     } catch (err) {
         res.status(404).json({message: err.message})
     }
