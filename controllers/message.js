f2ddd262f068e959d6e5bcbd2f96bdb34d0cfa5f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-25 16:22:13 -0600

    Create get messages controller

--- controllers/message.js
+++ controllers/message.js
@@ -14,3 +14,14 @@ export const newMessage = async (req, res) => {
     }
 }
 
+export const getMessages = async(req, res) => {
+    try {
+        const{ conversationId } = req.params
+        const messages = await Message.find({
+            conversationId
+        })
+        res.status(200).json(messages)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
