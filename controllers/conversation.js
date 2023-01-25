158419d79161cb9b30f1a312612dd056c589b53e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-25 16:08:05 -0600

    Create getConversation controller

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -1,4 +1,4 @@
-import Conversation from "../models/Conversation"
+import Conversation from "../models/Conversation.js"
 
 export const newConversation = async (req, res) => {
 
@@ -13,6 +13,22 @@ export const newConversation = async (req, res) => {
         const savedConversation = await conversation.save()
         res.status(201).json(savedConversation)
 
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
+export const getConversation = async(req, res) => {
+    try {
+
+        const {userId} = req.params;
+
+        const conversation = await Conversation.find({
+            members: {$in: [userId]}
+        });
+
+        res.status(200).json(conversation);
+        
     } catch (err) {
         res.status(500).json({message: err.message})
     }
